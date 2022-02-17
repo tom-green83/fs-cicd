ecea2d2395f1710766ff8ac1d9bf81454021d33c
Tom Green <tom.green83@protonmail.com>
2022-02-17 20:48:38 +0000

    Increase cypress test click rate

--- client/cypress/integration/blog_app.spec.js
+++ client/cypress/integration/blog_app.spec.js
@@ -132,11 +132,11 @@ describe('Blog app', function() {
         const finalLikes = [6, 5, 4]
         for (let i = 0; i <4; i++) {
           cy.get('.blog').contains(`${blog1Like.title} ${blog1Like.author}`).contains('like').click()
-          cy.wait(500)
+          cy.wait(2000)
         }
         for (let i = 0; i <4; i++) {
           cy.get('.blog').contains(`${blog2Like.title} ${blog2Like.author}`).contains('like').click()
-          cy.wait(500)
+          cy.wait(2000)
         }
         cy.get('.blog').contains(`${blog3Like.title} ${blog3Like.author}`).contains('like').click()
 
