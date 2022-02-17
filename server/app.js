28c5807590780bda8a43d283c9f02c5c62d4239b
Tom Green <tom.green83@protonmail.com>
2022-02-17 20:07:31 +0000

    Fix workflow

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('./client/build'))
+app.use(express.static('../client/build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
